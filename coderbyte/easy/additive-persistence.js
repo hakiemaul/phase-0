3f5f595091d414c561febb0c42a8210d3a826138
hakiemaul <noersedya@gmail.com>
2017-04-18 13:37:59 +0700

    added explanation on additive persistence

--- coderbyte/easy/additive-persistence.js
+++ coderbyte/easy/additive-persistence.js
@@ -1,4 +1,4 @@
-// Have the function AdditivePersistence(num) take the num parameter being passed 
+// Have the function AdditivePersistence(num) take the num parameter being passed
 // which will always be a positive integer and return its additive persistence
 // which is the number of times you must add the digits in num until you reach
 // a single digit. For example: if num is 2718 then your program should return
@@ -8,14 +8,19 @@
 // Test Case Point: 5
 
 function AdditivePersistence(num) {
+  // ubah menjadi string
   var strNum=num.toString();
   var hitung=0;
+  // iterasi hingga panjang string = 1
   while(strNum.length>1) {
     var jumlah=0;
+    // penjumlahan setiap angka di dalam string
     for(var i=0;i<strNum.length;i++) {
       jumlah+=Number(strNum[i]);
     }
+    // ubah hasil penjumlahan menjadi string
     strNum=jumlah.toString();
+    // hitung tiap iterasi
     hitung++;
   }
   return hitung;
