bfa07fa93511e01bb4d2bab1a3e58b8c713ce495
hakiemaul <noersedya@gmail.com>
2017-04-23 17:33:26 +0700

    added explanation and reducing the code length (3 lines)

--- coderbyte/medium/formatted-division.js
+++ coderbyte/medium/formatted-division.js
@@ -5,22 +5,27 @@
 
 "use strict"
 function FormattedDivision(num1,num2) {
+  // dividing the number
   let div=num1/num2;
   let arr=div.toString().split('');
   let tampung=[];
   let tes=0;
   let koma=0;
   let foo=0;
+  // finding coordinate of the '.' in array
   for(let i=0;i<arr.length;i++) {
     if(arr[i]=='.') {
       tes=i;
       koma=tes;
     }
   }
+  // giving '.' if the division gave a whole number result
   if(tes==0) {
     arr.push('.');
+    tes=arr.length-1;
     koma=arr.length-1;
   }
+  // make another array which only took up to 4 digits after the '.'
   for(let i=0;i<arr.length;i++) {
     if(i<=(koma+4)) {
       tampung[i]=arr[i];
@@ -28,13 +33,13 @@ function FormattedDivision(num1,num2) {
   }
   for(let i=0;i<arr.length;i++) {
     if(arr[i]=='.') {
-      tes=i;
-      koma=tes;
+      // gave the thousands denominator (',')
       while(tes>3) {
         tampung.splice(tes-3,0,',');
         tes-=3;
         koma+=1;
       }
+      // gave another zeroes if there is not enough zero behind '.'
       if(koma>tampung.length-5 && koma!=tampung.length-1) {
         foo=koma-(tampung.length-5);
         while(foo>0) {
@@ -45,9 +50,10 @@ function FormattedDivision(num1,num2) {
       if(koma==tampung.length-1) tampung.push('0000');
     }
   }
+  // rounding up the last digit if the condition is met
   if(Number(tampung[tampung.length-1])>=5) tampung[tampung.length-1]=Number(tampung[tampung.length-1])+1;
   console.log(tampung.join(''))
 }
 
 // keep this function call here
-FormattedDivision(9112,2);
+FormattedDivision(911200000000,3);
