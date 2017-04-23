409d2460d87a701ed0de12bf00ed1bc23a4d4317
hakiemaul <noersedya@gmail.com>
2017-04-23 07:37:57 +0700

    added binary reversal question

--- coderbyte/medium/binary-converter.js
+++ coderbyte/medium/binary-converter.js
@@ -1,3 +1,8 @@
+// Have the function BinaryConverter(str) return the decimal form of the binary value. For example: if 101 is passed return 5, or if 1000 is passed return 8.
+
+// By: Aulia Hakiem
+// Test Case Point: 5
+
 "use strict"
 function BinaryConverter(str) {
   let dec=parseInt(str, 2);
