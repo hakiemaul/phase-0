1407e3b22ee0e520e14f0a58483ce431d653853d
hakiemaul <noersedya@gmail.com>
2017-04-23 07:39:59 +0700

    added serial number question

--- coderbyte/easy/serial-number.js
+++ coderbyte/easy/serial-number.js
@@ -1,3 +1,15 @@
+// Have the function SerialNumber(str) take the str parameter being passed and determine if it is a valid serial number with the following constraints:
+//
+// 1. It needs to contain three sets each with three digits (1 through 9) separated by a period.
+// 2. The first set of digits must add up to an even number.
+// 3. The second set of digits must add up to an odd number.
+// 4. The last digit in each set must be larger than the two previous digits in the same set.
+//
+// If all the above constraints are met within the string, the your program should return the string true, otherwise your program should return the string false. For example: if str is "224.315.218" then your program should return "true".
+
+// By: Aulia Hakiem
+// Test Case Point: 5
+
 "use strict"
 function SerialNumber(str) {
   let arr=str.split('.');
